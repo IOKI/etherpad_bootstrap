23ab02844c25222e8f66b78caa58d356265188d5
Lukasz Wojciak <lukasz.wojciak@pearson.com>
2014-03-11 12:56:52 +0100

    +Move to IOKI repos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 etherpad_bootstrap
 ==================
+
+0. sh bootstrap.sh - do all the things
+1. grunt [default task] - gets content from repositories and starts the app
+2. grunt run - just run 
--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,47 @@
 module.exports = function (grunt) {
     'use strict';
 
-    grunt.initConfig({
-        gitclone: {
-            clone: {
-                options: {
-                    repository: 'https://github.com/ether/etherpad-lite.git',
-                    tag: '1.3.0',
-                    directory: 'app'
+    var plugins = grunt.file.readJSON('package.etherpad.json').dependencies,
+        repository = 'https://api.github.com/repos/IOKI/',
+        tasks = {
+            gitclone: {
+                clone: {
+                    options: {
+                        repository: 'https://github.com/IOKI/' + 'etherpad-lite.git',
+                        tag: '1.3.0',
+                        directory: 'app'
+                    }
+                }
+            },
+            copy: {
+                packagePlugins: {
+                    files: [
+                        {expand: false, src: ['package.etherpad.json'], dest: 'app/package.json'},
+                        {expand: false, src: ['settings.json.template'], dest: 'app/settings.json'}
+                    ]
+                }
+            },
+            shell: {
+                run: {
+                    options: {
+                        stdout: true,
+                        stderr: true,
+                        execOptions: {
+                            cwd: 'app'
+                        }
+                    },
+                    command: 'npm install && sh bin/run.sh'
                 }
             }
         },
-        copy: {
-            packagePlugins: {
-                files: [
-                    {expand: false, src: ['package.etherpad.json'], dest: 'app/package.json'},
-                    {expand: false, src: ['settings.json.template'], dest: 'app/settings.json'}
-                ]
-            }
-        },
-        shell: {
-            installPlugins: {
+        pluginArray = [],
+        plugin;
+
+    for (plugin in plugins) {
+        if (plugins.hasOwnProperty(plugin)) {
+            pluginArray.push(plugin);
+
+            tasks.shell[plugin] = {
                 options: {
                     stdout: true,
                     stderr: true,
@@ -28,23 +49,35 @@ module.exports = function (grunt) {
                         cwd: 'app'
                     }
                 },
-                command: 'npm install && sh bin/run.sh'
-            }
+                command: 'npm install ' + repository + plugin + '/tarball'
+            };
         }
-    });
+    }
+
+    grunt.initConfig(tasks);
 
     grunt.loadNpmTasks('grunt-git');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask('default', ['gitclone', 'copy'], function () {
-        var isCloned = grunt.file.exists('app');
+    grunt.registerTask('default', ['gitclone', 'copy', 'shell'], function () {
+        var isCloned = grunt.file.exists('app'),
+            runArray = ['shell:run'];
+
+        pluginArray.forEach(function (item) {
+            runArray.unshift('shell:' + item);
+        });
 
         if (isCloned) {
-            grunt.task.run(['copy', 'shell']);
+            grunt.task.run(['copy'].concat(runArray));
         } else {
-            grunt.task.run(['gitclone', 'copy', 'shell']);
+            grunt.task.run(['gitclone', 'copy'].concat(runArray));
         }
+
+    });
+
+    grunt.registerTask('run', ['shell'], function () {
+        grunt.task.run(['copy', 'shell:run'])
     });
 
 };
